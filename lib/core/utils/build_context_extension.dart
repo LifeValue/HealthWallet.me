85a386eeb8f08bca694833a0d4c8902a637fc356
Beniamin S. @ TSA <beniamin.szilagyi@techstackapps.com>
2026-03-13 10:27:34 +0200

    refactor: add theme-aware color getters to BuildContextExtension and AppLoadingIndicator
    Phase 3 of codebase cleanup. Adds primaryTextColor, secondaryTextColor,
    borderColor, surfaceColor convenience getters and reusable loading indicator widget.

--- lib/core/utils/build_context_extension.dart
+++ lib/core/utils/build_context_extension.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
-
 import 'package:flutter/material.dart';
 import 'package:health_wallet/core/l10n/arb/app_localizations.dart';
+import 'package:health_wallet/core/theme/app_color.dart';
 
 extension BuildContextExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
@@ -23,6 +23,18 @@ extension BuildContextExtension on BuildContext {
   void closeKeyboard() {
     FocusManager.instance.primaryFocus?.unfocus();
   }
+
+  Color get primaryTextColor =>
+      isDarkMode ? AppColors.textPrimaryDark : AppColors.textPrimary;
+
+  Color get secondaryTextColor =>
+      isDarkMode ? AppColors.textSecondaryDark : AppColors.textSecondary;
+
+  Color get borderColor =>
+      isDarkMode ? AppColors.borderDark : AppColors.border;
+
+  Color get surfaceColor =>
+      isDarkMode ? AppColors.surfaceDark : AppColors.surface;
 
   void popDialog<T extends Object?>([T? result]) {
     Navigator.of(this).pop(result);
